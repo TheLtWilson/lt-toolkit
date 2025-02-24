e11db9c52880b8d79096f9ed455eb359b5ce9e43
Lt. Wilson <riley@ltwilson.tv>
2025-02-24 15:07:30 -0500

    Added start of chat voice page

---
+++ app/chatvoice/page.tsx
@@ -0,0 +1,18 @@
+import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+
+export default function ChatVoice() {
+    return (
+        <div className="p-4 mx-auto max-w-4xl">
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-3xl">Chat Voice</CardTitle>
+                    <CardDescription>
+                        Give your Twitch chat the ability to speak! This utility will read out messages sent from a specified channel.
+                    </CardDescription>
+                </CardHeader>
+            </Card>
+
+            WIP
+        </div>
+    )
+}
